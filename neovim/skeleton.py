2d7b4b9a0284d387f54da84a1a03077fe166cfb5
LucasRochaAbraao <lucasrabraao@gmail.com>
2022-12-04 12:05:18 -0300

    refactor directory layout and implement install script

---
+++ neovim/skeleton.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+[
+    Documentation here
+            -
+    Made this to learn how
+    "skeletons" work in vim
+]
+
+Author: Lucas Rocha Abraão
+Date: [date here]
+License: GNU GPLv3
+"""
+
+
+def main():
+    """ Docs """
+    ...
+
+
+if __name__ == "__main__":
+    main()
